b6b7d9297a06955ab3a3dd2fe4c48ae471680ac3
Nathan Wallace <nathan.s.wallace@gmail.com>
2013-04-22 12:06:51 -0400

    finalized geolocation and added about page

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,13 +5,13 @@ class Pin < ActiveRecord::Base
   validates :description, presence: true
   validates_attachment :image, presence: true,
   															content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']},
-  															size: { less_than: 3.megabytes }
+  															size: { less_than: 7.megabytes }
 
 
   belongs_to :user
   has_attached_file :image, :styles => { :small => "214x214>", :medium => "500x500>", :large => "800x800>"}
 
-after_post_process :get_image_loc # unsure about this
+  after_post_process :get_image_loc # unsure about this
 
   def image_remote_url=(url_value)
     self.image = URI.parse(url_value) unless url_value.blank?
