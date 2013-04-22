e28d901a7b15e6ff956ed2a15065cbd9166283b6
Nathan Wallace <nathan.s.wallace@gmail.com>
2013-04-21 22:09:36 -0400

    more renames

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Badparkin::Application.config.secret_token = '5eea62322c67395e5cc38a8abfb304be03c01c83820436118e014fea47a8ad40c32d5cf9a9068ac00d21d0d865d320b00c6917943efd405c894e14b695a79d64'
+DCPhotoGrid::Application.config.secret_token = '5eea62322c67395e5cc38a8abfb304be03c01c83820436118e014fea47a8ad40c32d5cf9a9068ac00d21d0d865d320b00c6917943efd405c894e14b695a79d64'
