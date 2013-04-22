de1110c801be29e2aa7d0817f5a598e65d1122b4
Nathan Wallace <nathan.s.wallace@gmail.com>
2013-04-22 16:44:13 -0400

    pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,12 +1,12 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image, :approved, :image_remote_url
-  
-  validates :user_id, presence: true
-  validates :description, presence: true
-  validates_attachment :image, presence: true,
-  															content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']},
-  															size: { less_than: 10.megabytes }
+#creates getter and setter objects
+  attr_accessible :description, :image, :image_remote_url
 
+  validates :description, presence: true
+  validates :user_id, presence: true
+  validates_attachment :image, presence: true,
+   content_type: { content_type: ['image/JPG','image/jpeg','image/jpg','image/png','image/gif']},
+   size: { less_than: 10.megabytes }
 
   belongs_to :user
   has_attached_file :image, :styles => { :small => "214x214>", :medium => "500x500>", :large => "800x800>"}
@@ -33,4 +33,3 @@ class Pin < ActiveRecord::Base
     self.img_loc_lng = lng # imgfile.gps_longitude
   end
 end
-
