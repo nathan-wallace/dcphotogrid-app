91f010fe64702f4a9ee3ec57eefbf42f0ce5de89
Nathan Wallace <nathan.s.wallace@gmail.com>
2013-04-22 14:05:33 -0400

    Revert "model therapy"
    This reverts commit f6f7df994d4cb60ef9c5f37fa69ccd5479cb5169.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,12 +1,12 @@
 class Pin < ActiveRecord::Base
-#creates getter and setter objects
-  attr_accessible :description, :image, :image_remote_url
-
-  validates :description, presence: true
+  attr_accessible :description, :image, :approved, :image_remote_url
+  
   validates :user_id, presence: true
+  validates :description, presence: true
   validates_attachment :image, presence: true,
-   content_type: { content_type: ['image/JPG','image/jpeg','image/jpg','image/png','image/gif']},
-   size: { less_than: 5.megabytes }
+  															content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']},
+  															size: { less_than: 7.megabytes }
+
 
   belongs_to :user
   has_attached_file :image, :styles => { :small => "214x214>", :medium => "500x500>", :large => "800x800>"}
@@ -14,11 +14,11 @@ class Pin < ActiveRecord::Base
   after_post_process :get_image_loc # unsure about this
 
   def image_remote_url=(url_value)
-   self.image = URI.parse(url_value) unless url_value.blank?
-   super # existing functionality is inherited
+    self.image = URI.parse(url_value) unless url_value.blank?
+    super
   end
 
-  # Image geolocation
+# Image geolocation
   def get_image_loc
     imgfile = EXIFR::JPEG.new(image.queued_for_write[:original].path)
     return unless imgfile
@@ -26,10 +26,10 @@ class Pin < ActiveRecord::Base
     lat = imgfile.exif[0].gps_latitude[0].to_f + (imgfile.exif[0].gps_latitude[1].to_f / 60) + (imgfile.exif[0].gps_latitude[2].to_f / 3600)
     lng = imgfile.exif[0].gps_longitude[0].to_f + (imgfile.exif[0].gps_longitude[1].to_f / 60) + (imgfile.exif[0].gps_longitude[2].to_f / 3600)
 
-    lat = lat * -1 if imgfile.exif[0].gps_latitude_ref == "S" # (N is +, S is -)
-    lng = lng * -1 if imgfile.exif[0].gps_longitude_ref == "W" # (W is -, E is +)
+    lat = lat * -1 if imgfile.exif[0].gps_latitude_ref == "S"      # (N is +, S is -)
+    lng = lng * -1 if imgfile.exif[0].gps_longitude_ref == "W"   # (W is -, E is +)
 
-    self.img_loc_lat = lat # imgfile.gps_latitude
-    self.img_loc_lng = lng # imgfile.gps_longitude
+    self.img_loc_lat  = lat # imgfile.gps_latitude
+    self.img_loc_lng  = lng # imgfile.gps_longitude
   end
-end
+ end 
