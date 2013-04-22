468c70209323b53c546e93c074c4bcb4cb136484
Nathan Wallace <nathan.s.wallace@gmail.com>
2013-04-21 22:50:55 -0400

    trying to fix rake db migrate

--- db/migrate/20130404221121_remove_marking_emails_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveMarkingEmailsFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :marking_emails
-  end
-
-  def down
-    add_column :users, :marking_emails, :boolean
-  end
-end
