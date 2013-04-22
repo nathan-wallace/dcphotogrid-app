4d2799f120dc8f2ad5cb8662093ac8251bb25467
Nathan Wallace <nathan.s.wallace@gmail.com>
2013-04-22 16:47:42 -0400

    pin.rb

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ end
 
 module DCPhotoGrid
   class Application < Rails::Application
-
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
