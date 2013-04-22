ad56a2ea009d20b3347629076b14abdbf4bf1c79
Nathan Wallace <nathan.s.wallace@gmail.com>
2013-04-22 15:57:05 -0400

    suggested fixes for method nil

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.12'
 gem 'devise'
 gem 'simple_form'
+gem "rake", "~> 10.0.4"
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ end
 
 module DCPhotoGrid
   class Application < Rails::Application
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
