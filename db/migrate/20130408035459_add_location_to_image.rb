0926b1e414bf7e5850b82ae44d3a3e43e06381cb
nathan-wallace <nathan.s.wallace@gmail.com>
2013-04-27 17:23:38 -0400

    undid schema and migration tinker

--- db/migrate/20130408035459_add_location_to_image.rb
+++ db/migrate/20130408035459_add_location_to_image.rb
@@ -1,5 +1,4 @@
 class AddLocationToImage < ActiveRecord::Migration
   def change
-    add_column :pins, :img_loc_lat, :decimal, :default => "", :null => false
-    add_column :pins, :img_loc_lng, :decimal, :default => "", :null => false
-end
+    add_column :pins, :img_loc_lat, :decimal
+    add_column :pins, :img_loc_lng, :decimal
