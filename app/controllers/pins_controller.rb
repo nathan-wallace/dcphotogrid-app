a9d58df0e72459472165cee7a3192f1c636fbaf0
Nathan Wallace <nathan.s.wallace@gmail.com>
2013-04-29 20:40:51 -0400

    pin

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -16,7 +16,8 @@ class PinsController < ApplicationController
   # GET /pins/1
   # GET /pins/1.json
   def show
-    @pin = Pin.find(params[:id])
+    @pins = Pin.find(params[:id], approved: false).order("created_at desc")
+
 
     respond_to do |format|
       format.html # show.html.erb
